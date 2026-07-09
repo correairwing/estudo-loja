31179f2445662e38d472ecdcc18751af45dea2a3
Irwing Corrêa <irwingcorrea95@email.com>
2026-07-08 20:34:22 -0400

    adicionando projeto base

---
+++ estudo-loja/src/main/java/com/correairwing/estudo_loja/EstudoLojaApplication.java
@@ -0,0 +1,13 @@
+package com.correairwing.estudo_loja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoLojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoLojaApplication.class, args);
+	}
+
+}
---
+++ estudo-loja/src/test/java/com/correairwing/estudo_loja/EstudoLojaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.correairwing.estudo_loja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoLojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
