2aa4f97ae73dcee5977f6948520256beac67ae6c
Irwing Corrêa <irwingcorrea95@email.com>
2026-07-08 22:22:34 -0400

    add entity classes for Acesso, Endereco, Pessoa, PessoaFisica, and PessoaJuridica with JPA annotations

---
+++ estudo-loja/src/main/java/com/correairwing/estudo_loja/entity/PessoaFisica.java
@@ -0,0 +1,39 @@
+package com.correairwing.estudo_loja.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pessoa_fisica")
+@PrimaryKeyJoinColumn(name = "id")
+public class PessoaFisica extends Pessoa {
+
+    private static final long serialVersionUID = 1L;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    
+    private String dataNascimento;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    
+
+}
