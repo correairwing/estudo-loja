2908b2895076d073b80c9c3615a5582668508f22
Irwing Corrêa <irwingcorrea95@email.com>
2026-07-11 19:39:44 -0400

    feat: update Postgres version to 17 and add TipoEndereco enum with associated properties

--- estudo-loja/src/main/java/com/correairwing/estudo_loja/entity/Endereco.java
+++ estudo-loja/src/main/java/com/correairwing/estudo_loja/entity/Endereco.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 import javax.persistence.ConstraintMode;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,6 +14,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+
+import com.correairwing.estudo_loja.enums.TipoEndereco;
 
 @Entity
 @Table(name = "endereco")
@@ -35,6 +39,18 @@ public class Endereco implements Serializable {
     @ManyToOne(targetEntity = Pessoa.class)
     @JoinColumn(name = "pessoa_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT,name = "fk_endereco_pessoa"), referencedColumnName = "id")
     private Pessoa pessoa;
+
+    @Enumerated(EnumType.STRING)
+    private TipoEndereco tipoEndereco;
+
+
+    public TipoEndereco getTipoEndereco() {
+        return tipoEndereco;
+    }
+
+    public void setTipoEndereco(TipoEndereco tipoEndereco) {
+        this.tipoEndereco = tipoEndereco;
+    }
 
     public Long getId() {
         return id;
